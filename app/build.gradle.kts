535d6f9ca3f6216979fe51b18f8da012728fdda3
richarddstevenn <131933833+richarddstevenn@users.noreply.github.com>
2024-11-26 18:16:45 +0700

    CodeLab 11 SharedPreferences

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.picasso)
+    implementation(libs.gson)
 }
--- app/src/main/java/latrecycleview/c14220154/recyclerview/MainActivity.kt
+++ app/src/main/java/latrecycleview/c14220154/recyclerview/MainActivity.kt
@@ -2,6 +2,7 @@ package latrecycleview.c14220154.recyclerview
 
 import android.content.DialogInterface
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -13,15 +14,19 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var _nama : MutableList<String>
-    private lateinit var _deskripsi : MutableList<String>
-    private lateinit var _karakter : MutableList<String>
-    private lateinit var _gambar : MutableList<String>
+    private var _nama : MutableList<String> = emptyList<String>().toMutableList()
+    private var _deskripsi : MutableList<String> = emptyList<String>().toMutableList()
+    private var _karakter : MutableList<String> = emptyList<String>().toMutableList()
+    private var _gambar : MutableList<String> = emptyList<String>().toMutableList()
 
     private var arWayang = arrayListOf<wayang>()
-    private lateinit var _rvWayang : RecyclerView
+    private lateinit var _rvWayang: RecyclerView
+
+    lateinit var sp : SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,21 +37,43 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        sp = getSharedPreferences("dataSP", MODE_PRIVATE)
+        val gson = Gson()
+        val isiSP = sp.getString("spWayang", null)
+        val type = object : TypeToken<ArrayList<wayang>>() {}.type
+        if(isiSP != null){
+            arWayang = gson.fromJson(isiSP, type)
+        }
+
         _rvWayang = findViewById<RecyclerView>(R.id.rvWayang)
-        SiapkanData()
+        if (arWayang.size == 0) {
+            SiapkanData()
+        } else {
+            arWayang.forEach {
+                _gambar.add(it.foto)
+                _nama.add(it.nama)
+                _karakter.add(it.karakter)
+                _deskripsi.add(it.deskripsi)
+            }
+            arWayang.clear()
+        }
         TambahData()
         TampilkanData()
     }
-    fun SiapkanData(){
+
+    fun SiapkanData() {
         _nama = resources.getStringArray(R.array.namaWayang).toMutableList()
         _deskripsi = resources.getStringArray(R.array.deskripsiWayang).toMutableList()
         _karakter = resources.getStringArray(R.array.karakterUtamaWayang).toMutableList()
         _gambar = resources.getStringArray(R.array.gambarWayang).toMutableList()
     }
 
-    fun TambahData(){
+    fun TambahData() {
+        val gson = Gson()
+        val editor = sp.edit()
         arWayang.clear()
-        for (position in _nama.indices){
+        for (position in _nama.indices) {
             val data = wayang(
                 _gambar[position],
                 _nama[position],
@@ -55,15 +82,19 @@ class MainActivity : AppCompatActivity() {
             )
             arWayang.add(data)
         }
+        val json = gson.toJson(arWayang)
+        editor.putString("spWayang", json)
+        editor.apply()
     }
-    fun TampilkanData(){
-        val adapterWayang = adapterRecView(arWayang)
-        _rvWayang.adapter = adapterWayang
-        _rvWayang.layoutManager = LinearLayoutManager(this)
 
-        adapterWayang.setOnItemClickCallback(object  : adapterRecView.OnItemClickCallback{
+    fun TampilkanData() {
+        val adapterWayang = adapterRecView(arWayang)
+        _rvWayang.layoutManager = LinearLayoutManager(this)
+        _rvWayang.adapter = adapterWayang
+
+        adapterWayang.setOnItemClickCallback(object : adapterRecView.OnItemClickCallback {
             override fun onItemClicked(data: wayang) {
-                Toast.makeText(this@MainActivity, data.nama, Toast.LENGTH_LONG).show()
+                Toast.makeText(this@MainActivity, data.nama, Toast.LENGTH_SHORT).show()
                 val intent = Intent(this@MainActivity, detWayang::class.java)
                 intent.putExtra("kirimData", data)
                 startActivity(intent)
@@ -72,23 +103,20 @@ class MainActivity : AppCompatActivity() {
             override fun delData(pos: Int) {
                 AlertDialog.Builder(this@MainActivity)
                     .setTitle("Hapus Data")
-                    .setMessage("Apakah Benar Data " + _nama[pos] + " akan dihapus ?")
+                    .setMessage("Apakah Benar Data " + _nama[pos] + " akan dihapus?")
                     .setPositiveButton("HAPUS", DialogInterface.OnClickListener { dialog, which ->
                         _gambar.removeAt(pos)
-                        _nama.removeAt(pos)
                         _deskripsi.removeAt(pos)
                         _karakter.removeAt(pos)
+                        _nama.removeAt(pos)
                         TambahData()
                         TampilkanData()
                     })
                     .setNegativeButton("BATAL", DialogInterface.OnClickListener { dialog, which ->
-                        Toast.makeText(
-                            this@MainActivity,
-                            "Data Batal Dihapus",
-                            Toast.LENGTH_LONG
-                        ).show()
-                    }
-                    ).show()
+                        Toast.makeText(this@MainActivity, "Data Batal Hapus", Toast.LENGTH_SHORT)
+                            .show()
+                    })
+                    .show()
             }
         })
     }
